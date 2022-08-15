fee8191dec366704db3d7912d50620fe5019e128
Aiden Waldner <a.waldner@share.epsb.ca>
2022-08-14 19:07:14 -0600

    finished checkHand()

--- poker.py
+++ poker.py
@@ -6,12 +6,14 @@ from random import randint
 def checkHand(hand, river):
     cards = hand+river
     cards.sort(key=lambda x: x[2])
-    value = 0
+    value = cards[-1][2]
     totalPairs = []
     hearts = []
     spades = []
     clubs = []
     diamonds = []
+    straight = 0
+    mult = 0
     for card in cards:
         pairs = [card]
         others = cards.copy()
@@ -39,9 +41,10 @@ def checkHand(hand, river):
             if len(totalPairs) > 0:
                 found = False
                 for pairCard in pairs:
-                    for tpCard in totalPairs[0]:
-                        if pairCard == tpCard:
-                            found = True
+                    for i in range(len(totalPairs)):
+                        for tpCard in totalPairs[i]:
+                            if pairCard == tpCard:
+                                found = True
                 if found == False:
                     totalPairs.append(pairs)
             else:
@@ -50,12 +53,14 @@ def checkHand(hand, river):
         if len(totalPairs[0]) == 3 or len(totalPairs[1]) == 3: # full house
             if totalPairs[0][0][2] > totalPairs[1][0][2]:
                 high = totalPairs[0][0][2]
+                value = 13*6+high
             else:
                 high = totalPairs[1][0][2]
             value = 13*6+high
         else:
             if totalPairs[0][0][2] > totalPairs[1][0][2]: # 2 pair
                 high = totalPairs[0][0][2]
+                value = 13*2+high
             else:
                 high = totalPairs[1][0][2]
                 value = 13*2+high
@@ -68,24 +73,104 @@ def checkHand(hand, river):
                 value = 13*3+pairs[0][2]
             elif len(pairs) == 4: # 4 of a kind
                 value = 13*7+pairs[0][2]
-
+    # straight check 
+    for i in range(len(cards)):
+        if i+cards[0][2] == cards[i][2]:
+            straight += 1
+    if straight == 5:
+        value = 13*4+cards[-1][2]
     # suit related
     cards = [spades, clubs, hearts, diamonds]
-    print(cards)
+    for ele in cards:
+        ele.sort(key=lambda x: x[2])
+        if len(ele) == 5:
+            straight = 0
+            for i in range(len(ele)):
+                if i+ele[0][2] == ele[i][2]:
+                    straight += 1
+            if straight == 5 and ele[-1][2] == 13:
+                value = 13*9+13
+            elif straight == 5:
+                value = 13*8+ele[-1][2]
+            elif value < 13*6:
+                value = 13*5+ele[-1][2]
     return value
+def printValue(hand, river, value):
+    print(f'''
+hand: {hand}
+river: {river}
+value: {value}''')
 
-
-for i in range(1):
+a = 0
+b = 0
+c = 0
+d = 0
+e = 0
+f = 0
+g = 0
+h = 0
+ii = 0
+j = 0
+k = []
+for i in range(50000000):
     import cards
     deck = cards.makeDeck()
     cards.shuffle(deck)
     hand = [cards.draw(deck), cards.draw(deck)]
     river = [cards.draw(deck), cards.draw(deck), cards.draw(deck)]
     value = checkHand(hand, river)
-    if value > 13*7 and value < 13*8:
+    
+    if value > 13*0 and value <= 13*1 and a == 0:
         value = checkHand(hand, river)
-        print(f'''
-hand: {hand}
-river: {river}
-value: {value}''')
+        printValue(hand, river, value)
+        a = 1
+        k.append(a)
+    if value > 13*1 and value <= 13*2 and b == 0:
+        value = checkHand(hand, river)
+        printValue(hand, river, value)
+        b = 1
+        k.append(b)
+    if value > 13*2 and value <= 13*3 and c == 0:
+        value = checkHand(hand, river)
+        printValue(hand, river, value)
+        c = 1
+        k.append(c)
+    if value > 13*3 and value <= 13*4 and d == 0:
+        value = checkHand(hand, river)
+        printValue(hand, river, value)
+        d = 1
+        k.append(d)
+    if value > 13*4 and value <= 13*5 and e == 0:
+        value = checkHand(hand, river)
+        printValue(hand, river, value)
+        e = 1
+        k.append(e)
+    if value > 13*5 and value <= 13*6 and f == 0:
+        value = checkHand(hand, river)
+        printValue(hand, river, value)
+        f = 1
+        k.append(f)
+    if value > 13*6 and value <= 13*7 and g == 0:
+        value = checkHand(hand, river)
+        printValue(hand, river, value)
+        g = 1
+        k.append(g)
+    if value > 13*7 and value <= 13*8 and h == 0:
+        value = checkHand(hand, river)
+        printValue(hand, river, value)
+        h = 1
+        k.append(h)
+    if value > 13*8 and value <= 13*9 and ii == 0:
+        value = checkHand(hand, river)
+        printValue(hand, river, value)
+        ii = 1
+        k.append(ii)
+    if value > 13*9 and value <= 13*10 and j == 0:
+        value = checkHand(hand, river)
+        printValue(hand, river, value)
+        j = 1
+        k.append(j)
+    if len(k) == 10:
+        quit()
+
 

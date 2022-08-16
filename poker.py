b2aa24233c0a2d2d9fac05ee9c2b17c33dd58eba
Aiden Waldner <a.waldner@share.epsb.ca>
2022-08-16 02:14:04 -0600

    added checking hands when river longer & printing value

--- poker.py
+++ poker.py
@@ -101,6 +101,69 @@ hand: {hand}
 river: {river}
 value: {value}''')
 
+def checkLarge(hand, river):
+    if len(river) == 3:
+        value = checkHand(hand, river)
+    elif len(river) == 4:
+        values = []
+        cards = hand+river
+        for card in cards:
+            others = cards.copy()
+            others.pop(others.index(card))
+            values.append(checkHand(others[:2], others[2:]))
+        vales.sort()
+        value = values[-1]
+    elif len(river) == 5:
+        values = []
+        cards = hand+river
+        for card in cards:
+            others = cards.copy()
+            others.pop(others.index(card))
+            for otherCard in others:
+                others = cards.copy()
+                others.pop(others.index(otherCard))
+                values.append(checkHand(others[:2], others[2:]))
+        values.sort()
+        value = values[-1]
+    return value
+
+def printValueNice(value):
+    hands = {
+            13: "Pair",
+            26: "Two Pair",
+            39: "Three of a Kind",
+            52: "Straight",
+            65: "Flush",
+            78: "Full House",
+            91: "Four of a Kind",
+            104: "Straight Flush",
+            117: "Royal Flush"
+    }
+    names = {
+            10: "Jack",
+            11: "Queen",
+            12: "King",
+            13: "Ace"
+    }
+    handValue = (value-(value%13))
+    high = value%13
+    if high != 0 and value > 13:
+        if high > 9:
+            high = names[high]
+        else:
+            high += 1
+        print(f"{hands[handValue]} with {high} high")
+    elif value > 13:
+        handValue -= 13
+        print(f"{hands[handValue]} with Ace high")
+    else:
+        if high > 9:
+            high = names[high]
+        elif high == 0:
+            high = "Ace"
+        else:
+            high += 1
+        print(f"{high} high")
 a = 0
 b = 0
 c = 0
@@ -117,57 +180,67 @@ for i in range(50000000):
     deck = cards.makeDeck()
     cards.shuffle(deck)
     hand = [cards.draw(deck), cards.draw(deck)]
-    river = [cards.draw(deck), cards.draw(deck), cards.draw(deck)]
-    value = checkHand(hand, river)
+    river = [cards.draw(deck), cards.draw(deck), cards.draw(deck), cards.draw(deck), cards.draw(deck)]
+    value = checkLarge(hand, river)
     
     if value > 13*0 and value <= 13*1 and a == 0:
-        value = checkHand(hand, river)
+        value = checkLarge(hand, river)
         printValue(hand, river, value)
+        printValueNice(value)
         a = 1
         k.append(a)
     if value > 13*1 and value <= 13*2 and b == 0:
-        value = checkHand(hand, river)
+        value = checkLarge(hand, river)
         printValue(hand, river, value)
+        printValueNice(value)
         b = 1
         k.append(b)
     if value > 13*2 and value <= 13*3 and c == 0:
-        value = checkHand(hand, river)
+        value = checkLarge(hand, river)
         printValue(hand, river, value)
+        printValueNice(value)
         c = 1
         k.append(c)
     if value > 13*3 and value <= 13*4 and d == 0:
-        value = checkHand(hand, river)
+        value = checkLarge(hand, river)
         printValue(hand, river, value)
+        printValueNice(value)
         d = 1
         k.append(d)
     if value > 13*4 and value <= 13*5 and e == 0:
-        value = checkHand(hand, river)
+        value = checkLarge(hand, river)
         printValue(hand, river, value)
+        printValueNice(value)
         e = 1
         k.append(e)
     if value > 13*5 and value <= 13*6 and f == 0:
-        value = checkHand(hand, river)
+        value = checkLarge(hand, river)
         printValue(hand, river, value)
+        printValueNice(value)
         f = 1
         k.append(f)
     if value > 13*6 and value <= 13*7 and g == 0:
-        value = checkHand(hand, river)
+        value = checkLarge(hand, river)
         printValue(hand, river, value)
+        printValueNice(value)
         g = 1
         k.append(g)
     if value > 13*7 and value <= 13*8 and h == 0:
-        value = checkHand(hand, river)
+        value = checkLarge(hand, river)
         printValue(hand, river, value)
+        printValueNice(value)
         h = 1
         k.append(h)
     if value > 13*8 and value <= 13*9 and ii == 0:
-        value = checkHand(hand, river)
+        value = checkLarge(hand, river)
         printValue(hand, river, value)
+        printValueNice(value)
         ii = 1
         k.append(ii)
     if value > 13*9 and value <= 13*10 and j == 0:
-        value = checkHand(hand, river)
+        value = checkLarge(hand, river)
         printValue(hand, river, value)
+        printValueNice(value)
         j = 1
         k.append(j)
     if len(k) == 10:
